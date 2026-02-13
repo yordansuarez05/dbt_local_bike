09da674b998fc1f34e6babed90a8bd9e885667b2
Yordan SUAREZ OROPEZA <yordanrsuarez@gmail.com>
2026-02-13 11:30:34 +0000

    price_category segmentation to dim_products

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -1,8 +1,19 @@
+-- dim_products
+-- Product dimension table for BI analysis
+-- Includes price segmentation for reporting
+
 select distinct
+
     product_id,
     product_name,
     category_name,
     brand_name,
-    round(list_price, 2) as list_price
+    round(list_price, 2) as list_price,
+
+    case 
+        when list_price < 500 then 'Low'
+        when list_price between 500 and 2000 then 'Medium'
+        else 'High'
+    end as price_category
 
 from {{ ref('int_sales_enriched') }}
