36d448b82ce3bdc2ef8881455b1e817a24d0fed2
Yordan SUAREZ OROPEZA <yordanrsuarez@gmail.com>
2026-02-13 11:57:04 +0000

    Improve price_category logic with null and negative price handling

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -1,8 +1,18 @@
 select distinct
+
     product_id,
     product_name,
     category_name,
     brand_name,
-    round(list_price, 2) as list_price
+    round(list_price, 2) as list_price,
+
+case
+    when list_price is null then 'Unknown'
+    when list_price < 0 then 'Invalid'
+    when list_price < 500 then 'Low'
+    when list_price between 500 and 1500 then 'Medium'
+    else 'High'
+end as price_category
+
 
 from {{ ref('int_sales_enriched') }}
