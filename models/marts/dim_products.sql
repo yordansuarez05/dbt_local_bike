8e66031f1895a733d512b0a4f3fa17cf511dd99f
Yordan SUAREZ OROPEZA <yordanrsuarez@gmail.com>
2026-02-13 11:23:26 +0000

    Refactor dim_products structure and documentation

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -1,8 +1,18 @@
+-- dim_products
+-- Product dimension for BI analysis
+-- Contains product attributes used for slicing sales
+
 select distinct
+
+    -- Primary key
     product_id,
+
+    -- Descriptive attributes
     product_name,
     category_name,
     brand_name,
+
+    -- Pricing
     round(list_price, 2) as list_price
 
 from {{ ref('int_sales_enriched') }}
